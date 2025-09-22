f88724dd53ad28d4c2d8cfc196ea0718a43bbd5b
Antonio Giner <calamity15khz@gmail.com>
2025-09-22 07:55:35 +0200

    Skip in-game invoking of Switchres when in windowed mode.

--- src/osd/windows/window.cpp
+++ src/osd/windows/window.cpp
@@ -804,7 +804,7 @@ void win_window_info::update()
 
 	// check if we need to change the video mode
 	auto &options = downcast<windows_options &>(machine().options());
-	if (options.switch_res() && options.changeres())
+	if (fullscreen() && options.switch_res() && options.changeres())
 		reset_required = WINOSD(machine())->switchres()->check_resolution_change(index(), monitor(), target(), &m_win_config);
 
 	// check if frame delay has changed
@@ -1832,7 +1832,7 @@ void win_window_info::adjust_window_position_after_major_change()
 void win_window_info::set_fullscreen(int fullscreen)
 {
 	assert(GetCurrentThreadId() == window_threadid);
-
+osd_printf_info("Set FULLSCREEN(%d)\n", fullscreen);
 	// if we're in the right state, punt
 	if (this->fullscreen() == fullscreen)
 		return;
@@ -1840,7 +1840,7 @@ void win_window_info::set_fullscreen(int fullscreen)
 
 	// kill off the renderer
 	renderer_reset();
-
+osd_printf_info("Set FULLSCREEN(%d)\n", fullscreen);
 	// hide ourself
 	ShowWindow(platform_window(), SW_HIDE);
 
