ab2178682aa10e8c5fa37f070718aaed966aaf50
Antonio Giner <calamity15khz@gmail.com>
2025-09-22 19:47:05 +0200

    Error checking and clean-up.

--- src/osd/windows/window.cpp
+++ src/osd/windows/window.cpp
@@ -1838,7 +1838,7 @@ void win_window_info::adjust_window_position_after_major_change()
 void win_window_info::set_fullscreen(int fullscreen)
 {
 	assert(GetCurrentThreadId() == window_threadid);
-osd_printf_info("Set FULLSCREEN(%d)\n", fullscreen);
+
 	// if we're in the right state, punt
 	if (this->fullscreen() == fullscreen)
 		return;
@@ -1846,7 +1846,7 @@ osd_printf_info("Set FULLSCREEN(%d)\n", fullscreen);
 
 	// kill off the renderer
 	renderer_reset();
-osd_printf_info("Set FULLSCREEN(%d)\n", fullscreen);
+
 	// hide ourself
 	ShowWindow(platform_window(), SW_HIDE);
 
