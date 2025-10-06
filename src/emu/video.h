2966fe1db00bbbd7c1c3d114b7109dc32687c14c
Antonio Giner <calamity15khz@gmail.com>
2025-10-06 20:15:31 +0200

    Add frame delay to speed % text.

--- src/emu/video.h
+++ src/emu/video.h
@@ -56,6 +56,7 @@ public:
 	bool sync_refresh() const { return m_syncrefresh; }
 	bool sync_audio() const { return m_syncaudio; }
 	int32_t framedelay() const { return m_framedelay; }
+	float auto_framedelay() const { return m_auto_framedelay; }
 	int32_t vsync_offset() const { return m_vsync_offset; }
 	bool fastforward() const { return m_fastforward; }
 
@@ -69,6 +70,7 @@ public:
 	void set_sync_refresh(bool syncrefresh) { m_syncrefresh = syncrefresh; }
 	void set_sync_audio(bool syncaudio) { m_syncaudio = syncaudio; }
 	void set_framedelay(int framedelay) { m_framedelay = framedelay; }
+	void set_auto_framedelay(float auto_framedelay) { m_auto_framedelay = auto_framedelay; }
 	void set_vsync_offset(int vsync_offset) { m_vsync_offset = vsync_offset; }
 
 	// misc
@@ -153,6 +155,7 @@ private:
 	bool                m_syncrefresh;              // flag: TRUE if we're currently refresh-synced
 	bool                m_syncaudio;                // flag: TRUE if audio resampling is enabled
 	int32_t             m_framedelay;               // tenths of frame to delay emulation start
+	float               m_auto_framedelay;          // current automatic frame delay value
 	int32_t             m_vsync_offset;             // offset vsync position by this many lines
 	bool                m_fastforward;              // flag: true if we're currently fast-forwarding
 	u32                 m_seconds_to_run;           // number of seconds to run before quitting
