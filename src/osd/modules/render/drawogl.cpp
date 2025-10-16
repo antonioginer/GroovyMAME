1c6f6e8010646b8c220720391a69c441e9c222da
Antonio Giner <calamity15khz@gmail.com>
2025-10-16 11:37:20 +0200

    Fix timestamp computation, osd_ticks() does not return the same clock source as drmCrtcGetSequence. Properly account for negative timestamp deltas, the kernel sometimes returns timestamps that are slightly in the future.

--- src/osd/modules/render/drawogl.cpp
+++ src/osd/modules/render/drawogl.cpp
@@ -1758,23 +1758,21 @@ int renderer_ogl::draw(const int update)
 	uint64_t ns = 0;
 
 	int ret = drmCrtcGetSequence(fd, crtc_id, &sequence, &ns);
-	if (ret == 0)
-		osd_printf_verbose("sequence: %d frames; timestamp: %ld ns\n", sequence, ns);
-	else
-		osd_printf_verbose("drmCrtcGetSequence: %d\n", ret);
+	if (ret != 0)
+		osd_printf_info("error: drmCrtcGetSequence(%d)\n", ret);
 
 	m_sync.register_vblank_in_ns(sequence, ns);
 
 	raster_status raster = {};
 	m_sync.get_raster(&raster);
-	osd_printf_verbose("[%.3f] get raster->[%d][%.3f] ", time_now(), raster.count, raster.scan);
+	osd_printf_info("[%.3f] get raster->[%d][%.3f] ", time_now(), raster.count, raster.scan);
 
 	missed_previous_retrace = raster.count > sync_frame;
 
 	if (!missed_previous_retrace)
-		wait_before = m_sync.wait_raster(raster.count, 0.70);
+		wait_before = m_sync.wait_raster(raster.count, 0.90);
 	else
-		osd_printf_verbose("missed retrace\n");
+		osd_printf_info("missed retrace\n");
 
 /*
 #ifdef SDLMAME_X11
@@ -1787,17 +1785,19 @@ int renderer_ogl::draw(const int update)
 
 	m_gl_context->swap_buffer();
 
+	m_sync.register_tag(raster_sync::AFTER_PRESENT);
+
 /*
 #ifdef SDLMAME_X11
 	// wait for vertical retrace
 	if ((video_config.sync_mode == 1 || video_config.sync_mode == 2) && video_config.syncrefresh && fd)
 		drm_waitvblank(window().monitor()->oshandle());
 #endif
-*/
+
 
 	// Finish GL to minimize latency
-	//glFinish();
-
+	glFinish();
+*/
 
 	if (video_config.framedelay == 0)
 	{
@@ -1809,14 +1809,11 @@ int renderer_ogl::draw(const int update)
 		// user defined
 		m_frame_delay = (double)(video_config.framedelay) / 10.0;
 
-	osd_printf_verbose("[%.3f] ", get_ms(osd_ticks() - m_time_start));
+	osd_printf_info("[%.3f] ", get_ms(osd_ticks() - m_time_start));
 	sync_frame = raster.count + (missed_previous_retrace? 0 : 1);
 	wait_after = m_sync.wait_raster(sync_frame, m_frame_delay);
 
-	osd_printf_verbose("[%.3f] wait: %.3f ", get_ms(osd_ticks() - m_time_start), get_ms((wait_before + wait_after) / 100));
-	m_sync.register_tag(raster_sync::AFTER_DRAW);
-
-
+	osd_printf_info("[%.3f] wait: %.3f ", get_ms(osd_ticks() - m_time_start), get_ms(wait_before + wait_after));
 	m_sync.register_tag(raster_sync::AFTER_DRAW);
 
 	return 0;
