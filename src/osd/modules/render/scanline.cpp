14f16823f2e6e22b892a4baf76d36d35149a6ff7
Antonio Giner <calamity15khz@gmail.com>
2025-11-05 22:51:21 +0100

    More emusync centralization. Emusync osd modules WIP.

--- src/osd/modules/render/scanline.cpp
+++ src/osd/modules/render/scanline.cpp
@@ -12,6 +12,7 @@
 #include <thread>
 #include "scanline.h"
 #include "emu.h"
+#include "emusync.h"
 
 // Windows SDK type definitions
 
@@ -50,90 +51,70 @@ D3DKMT_GET_SCANLINE GetScanline;
 D3DKMT_OPEN_ADAPTER_FROM_HDC OpenAdapterFromHdc;
 D3DKMT_WAIT_FOR_VERTICAL_BLANK_EVENT WaitForVerticalBlankEvent;
 
-// osd module data
-D3DKMT_OPENADAPTERFROMHDC adapter_data;
-D3DKMT_GETSCANLINE scanline_data;
-D3DKMT_WAITFORVERTICALBLANKEVENT vblank_data;
-
 // static variables
+static D3DKMT_OPENADAPTERFROMHDC adapter_data;
 static std::thread scan_poll;
 static uint64_t vblank_timestamp = 0;
 static uint64_t vblank_counter = 0;
 static bool is_active = false;
 static bool is_initialized = false;
 
+bool scanline_init(uint64_t monitor_handle, bool polling_thread);
+void scanline_poll(uint32_t *scanline, bool *in_vblank);
+
 
 //============================================================
-//  scanline_poll
+//  emusync:init_osd
 //============================================================
 
-void scanline_poll(uint32_t *scanline, bool *in_vblank)
+void emusync::osd_init(uint64_t monitor_handle, std::function<bool(void)> get_vblank_timestamp_external, std::function<uint64_t(void)> get_frame_counter_external)
 {
-	// Poll new values
+	get_vblank_timestamp = get_vblank_timestamp_external == nullptr ?
+						std::bind(&emusync::get_vblank_timestamp_default, this) :
+						get_vblank_timestamp_external;
 
-	scanline_data.hAdapter = adapter_data.hAdapter;
-	scanline_data.VidPnSourceId = adapter_data.VidPnSourceId;
-	if ((*GetScanline)(&scanline_data) == STATUS_SUCCESS)
+	get_frame_counter = get_frame_counter_external;
+
+	// If the renderer doesn't have a timestamp method, use Windows to get timestamps
+	bool use_polling_thread = (get_vblank_timestamp_external == nullptr);
+
+	scanline_init(monitor_handle, use_polling_thread);
+}
+
+
+//============================================================
+//  emusync::deinit
+//============================================================
+
+void emusync::osd_deinit()
+{
+	if (is_active)
 	{
-		if (scanline != nullptr) *scanline = scanline_data.ScanLine;
-		if (in_vblank != nullptr) *in_vblank = scanline_data.InVerticalBlank;
+		is_active = false;
+		scan_poll.join();
 	}
 }
 
 
 //============================================================
-//  scanline_poll
+//  emusync::get_vblank_timestamp_default
 //============================================================
 
-void wait_for_vertical_blank()
+bool emusync::get_vblank_timestamp_default()
 {
-	vblank_data.hAdapter = adapter_data.hAdapter;
-	vblank_data.VidPnSourceId = adapter_data.VidPnSourceId;
-	if ((*WaitForVerticalBlankEvent)(&vblank_data) == STATUS_SUCCESS)
-	{
-		struct timespec monotime;
-		clock_gettime(CLOCK_MONOTONIC, &monotime);
-		vblank_timestamp = (uint64_t)(monotime.tv_sec) * (uint64_t)1000000000 + (uint64_t)(monotime.tv_nsec);
-		vblank_counter ++;
-		is_initialized = true;
-	}
-}
+	if (is_initialized)
+		register_vblank_in_ns(vblank_counter, vblank_timestamp);
 
-
-//============================================================
-//  polling thread
-//============================================================
-
-static void vblank_poll()
-{
-	osd_printf_verbose("emusync: polling thread started.\n");
-	is_active = true;
-
-	while (is_active)
-		wait_for_vertical_blank();
-
-	osd_printf_verbose("emusync: polling thread destroyed\n");
-}
-
-
-//============================================================
-//  get_vblank_timestamp_external
-//============================================================
-
-bool get_vblank_timestamp_external(uint64_t *counter, uint64_t *timestamp)
-{
-	*counter = vblank_counter;
-	*timestamp = vblank_timestamp;
 	return is_initialized;
 }
+
 
 //============================================================
 //  scanline_init
 //============================================================
 
-bool scanline_init(const char *output_name)
+bool scanline_init(uint64_t monitor_handle, bool polling_thread)
 {
-
 	// Get api function hooks
 	HINSTANCE hDLL;
 	hDLL = LoadLibraryA("gdi32.dll");
@@ -149,9 +130,14 @@ bool scanline_init(const char *output_name)
 	WaitForVerticalBlankEvent = (D3DKMT_WAIT_FOR_VERTICAL_BLANK_EVENT)GetProcAddress(hDLL,"D3DKMTWaitForVerticalBlankEvent");
 	if (WaitForVerticalBlankEvent == NULL) return false;
 
+	MONITORINFOEX mi;
+	mi.cbSize = sizeof(mi);
+	if (!GetMonitorInfo((HMONITOR)monitor_handle, &mi))
+		return false;
+
 	// Get adapter from device name
 	HDC hdc;
-	hdc = CreateDCA(NULL, output_name, NULL, NULL);
+	hdc = CreateDC(NULL, mi.szDevice, NULL, NULL);
 	if (hdc == NULL)
 		return false;
 
@@ -164,22 +150,54 @@ bool scanline_init(const char *output_name)
 	}
 	DeleteDC(hdc);
 
+	if (!polling_thread)
+		return true;
+
 	// Create polling thread
-	scan_poll = std::thread(&vblank_poll);
+	scan_poll = std::thread([]()
+	{
+		osd_printf_verbose("emusync: polling thread started.\n");
+		is_active = true;
+
+		while (is_active)
+		{
+			D3DKMT_WAITFORVERTICALBLANKEVENT vblank_data;
+			vblank_data.hAdapter = adapter_data.hAdapter;
+			vblank_data.hDevice = 0;
+			vblank_data.VidPnSourceId = adapter_data.VidPnSourceId;
+
+			if ((*WaitForVerticalBlankEvent)(&vblank_data) == STATUS_SUCCESS)
+			{
+				struct timespec monotime;
+				clock_gettime(CLOCK_MONOTONIC, &monotime);
+				vblank_timestamp = (uint64_t)(monotime.tv_sec) * (uint64_t)1000000000 + (uint64_t)(monotime.tv_nsec);
+				vblank_counter ++;
+				is_initialized = true;
+			}
+		}
+
+		osd_printf_verbose("emusync: polling thread destroyed\n");
+	});
 
 	return true;
 }
 
 
 //============================================================
-//  scanline_exit
+//  scanline_poll
 //============================================================
 
-void scanline_exit()
+void scanline_poll(uint32_t *scanline, bool *in_vblank)
 {
-	if (is_active)
+	// Poll new values
+	D3DKMT_GETSCANLINE scanline_data;
+	scanline_data.hAdapter = adapter_data.hAdapter;
+	scanline_data.VidPnSourceId = adapter_data.VidPnSourceId;
+
+	if ((*GetScanline)(&scanline_data) == STATUS_SUCCESS)
 	{
-		is_active = false;
-		scan_poll.join();
+		if (scanline != nullptr) *scanline = scanline_data.ScanLine;
+		if (in_vblank != nullptr) *in_vblank = scanline_data.InVerticalBlank;
 	}
 }
+
