a064ef6afa6a2e7676ae969ac70f9c5e369876ed
Antonio Giner <calamity15khz@gmail.com>
2025-11-05 22:51:21 +0100

    More emusync centralization. Emusync osd modules WIP.

--- src/osd/modules/render/scanline.h
+++ src/osd/modules/render/scanline.h
@@ -8,8 +8,4 @@
 
 #pragma once
 
-bool scanline_init(const char *output_name);
-void scanline_exit();
-bool get_vblank_timestamp_external(uint64_t *counter, uint64_t *timestamp);
-void wait_for_vertical_blank();
 void scanline_poll(uint32_t *scanline, bool *in_vblank);
