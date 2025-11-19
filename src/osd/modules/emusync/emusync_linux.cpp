0ce824cc009d01c24c11ccbac767db2fbe2e85cd
Antonio Giner <calamity15khz@gmail.com>
2025-11-19 13:19:27 +0100

    Remove temporary workaround for interlaced modes on Linux.

--- src/osd/modules/emusync/emusync_linux.cpp
+++ src/osd/modules/emusync/emusync_linux.cpp
@@ -40,8 +40,6 @@ bool emusync::osd_init(uint64_t monitor_handle, std::function<bool(void)> get_vb
 
 	get_frame_counter = get_frame_counter_external;
 
-	m_count_div = dynamic_cast<sdl_options const &>(machine().options()).interlace_force_even();
-
 	display_manager *display = downcast<sdl_osd_interface&>(machine().osd()).switchres()->switchres().display(0);
 	if (display != nullptr)
 	{
@@ -88,10 +86,6 @@ bool emusync::get_vblank_timestamp_default()
 		osd_printf_verbose("error: drmCrtcGetSequence(%d)\n", ret);
 		return false;
 	}
-
-	// Sync counts increase by 2 on interlaced modes. Normalize
-	if (interlaced())
-		sequence = sequence >> m_count_div;
 
 	register_vblank_in_ns(sequence, ns);
 
