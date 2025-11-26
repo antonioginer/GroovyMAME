8202b767dfe1f09c05ef4f4a14298c513f2d1787
Antonio Giner <calamity15khz@gmail.com>
2025-11-26 08:13:47 +0100

    Make predraw sync aware of vactive/vtotal ratio: synchronize blit with first line of vblank. Improve vtotal detection on Windows.

--- src/osd/modules/switchres/switchres_module.cpp
+++ src/osd/modules/switchres/switchres_module.cpp
@@ -367,7 +367,7 @@ bool switchres_module::set_mode(int i, osd_monitor_info *monitor, render_target 
 		}
 
 		set_options(display, target);
-		machine().sync().set_vtotal(display->selected_mode()->vtotal? display->selected_mode()->vtotal : display->selected_mode()->vactive);
+		machine().sync().set_vratio(display->selected_mode()->vactive, display->selected_mode()->vtotal);
 		machine().sync().set_interlace(display->is_interlaced());
 
 		return true;
