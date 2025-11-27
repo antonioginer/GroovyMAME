66b8586731260685d7fe4a9f918939ba8523ff42
intealls <inte@alls>
2025-11-27 04:53:33 +0100

    Cleanup PART and add back manual api and device selection, and only allow a single device Fix Windows compile issues and show only real-time capable APIs Output more stream details

--- src/osd/modules/lib/osdobj_common.h
+++ src/osd/modules/lib/osdobj_common.h
@@ -82,8 +82,9 @@
 #define OSDOPTION_SOUND                 "sound"
 #define OSDOPTION_AUDIO_LATENCY         "audio_latency"
 
+#define OSDOPTION_PART_API              "part_api"
+#define OSDOPTION_PART_DEVICE           "part_device"
 #define OSDOPTION_PART_LATENCY          "part_latency"
-#define OSDOPTION_PART_LOG              "part_log"
 
 #define OSDOPTION_AUDIO_OUTPUT          "audio_output"
 #define OSDOPTION_AUDIO_EFFECT          "audio_effect"
@@ -243,8 +244,9 @@ public:
 	// sound options
 	const char *sound() const { return value(OSDOPTION_SOUND); }
 	float audio_latency() const { return float_value(OSDOPTION_AUDIO_LATENCY); }
+	const char *part_api() const { return value(OSDOPTION_PART_API); }
+	const char *part_device() const { return value(OSDOPTION_PART_DEVICE); }
 	float part_latency() const { return float_value(OSDOPTION_PART_LATENCY); }
-	bool part_log() const { return bool_value(OSDOPTION_PART_LOG); }
 
 	// CoreAudio specific options
 	const char *audio_output() const { return value(OSDOPTION_AUDIO_OUTPUT); }
